da0adb2da97e9a738b07918722eec680c36104a4
billy <a.billy@pycentric.net>
2024-07-03 10:03:43 +0200

    add : added get_api methods

--- pytest_tutorials_and_notes/source/service.py
+++ pytest_tutorials_and_notes/source/service.py
@@ -1,3 +1,6 @@
+import requests
+
+
 ## creating a dummy DB
 
 database = {
@@ -9,3 +12,22 @@ database = {
 # Create a function which will get user from db , using user_id
 def get_user_from_db(user_id):
     return database.get(user_id)
+
+
+# Get users from an API
+def get_user():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if response.status_code == 200:
+        return response.json()
+    
+    # if not 200 raise HTTPError
+    raise requests.HTTPError
+
+# getting comments using an API
+def get_comments():
+    response = requests.get("https://jsonplaceholder.typicode.com/comments")
+    if (response.status_code == 200):
+        return response.json()
+    
+    # arise an error if the request was not successful
+    raise requests.HTTPError
--- pytest_tutorials_and_notes/test/test_service.py
+++ pytest_tutorials_and_notes/test/test_service.py
@@ -1,6 +1,8 @@
 import pytest
 import source.service as service
 import unittest.mock as mock
+import requests
+
 
 # write a test to get user from db
 #use decorater mock.patch("path")
@@ -10,3 +12,23 @@ def test_get_user_from_db(any_var):
     user_name = service.get_user_from_db(2)
 
     assert user_name == "Two"  
+
+@mock.patch("requests.get")
+def test_get_user(mock_get):
+    mock_reponse = mock.Mock()
+    mock_reponse.status_code = 200
+    mock_reponse.json.return_value = {"id" : 1,"name" : "John Doe"}
+    mock_get.return_value = mock_reponse
+    data = service.get_user()
+    
+    assert data == mock_reponse.json.return_value
+
+@mock.patch("requests.get")
+def test_get_comments(any_var):
+    mock_res = mock.Mock()
+    mock_res.status_code = 200
+    mock_res.json.return_value = {"postId": 1,"id": 1,}
+    any_var.return_value = mock_res
+    data = service.get_comments()
+
+    assert data == mock_res.json.return_value
