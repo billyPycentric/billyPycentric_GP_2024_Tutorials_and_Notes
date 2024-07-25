20ce82ee897616335a725fb73d4c4a51f6022f97
abongile <abongile@Pycentric.net>
2024-07-25 15:57:58 +0200

    create : added the secrets test func

--- streamlit_multipage_app/app_testing/main.py
+++ streamlit_multipage_app/app_testing/main.py
@@ -2,11 +2,11 @@
 import streamlit as st
 
 # Initialize st.session_state.beans
-st.session_state.beans = st.session_state.get("beans", 3)
+st.session_state.beans = st.session_state.get("beans", 6)
 
 st.title("Bean counter :paw_prints:")
 
-addend = st.number_input("Beans to add", 0, 10)
+addend = st.number_input("Beans to add", 7, 598)
 if st.button("Add"):
     st.session_state.beans += addend
 st.write(f"Beans counted: {st.session_state.beans}")
