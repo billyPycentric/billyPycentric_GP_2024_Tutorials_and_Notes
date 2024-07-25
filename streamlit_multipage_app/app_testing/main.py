54597712c40e2e708ba30c3f220145206ea38cbe
billy <a.billy@pycentric.net>
2024-07-25 11:23:12 +0200

    delete : Deletd all the unnecessary files and dirs

--- streamlit_multipage_app/app_testing/main.py
+++ streamlit_multipage_app/app_testing/main.py
@@ -1,9 +1,12 @@
+"""app.py"""
 import streamlit as st
 
-st.session_state.beans = st.session_state.get("beans",0)
+# Initialize st.session_state.beans
+st.session_state.beans = st.session_state.get("beans", 0)
+
 st.title("Bean counter :paw_prints:")
 
 addend = st.number_input("Beans to add", 0, 10)
 if st.button("Add"):
     st.session_state.beans += addend
-st.markdown(f"Beans counted: {st.session_state.beans}")
+st.write(f"Beans counted: {st.session_state.beans}")
